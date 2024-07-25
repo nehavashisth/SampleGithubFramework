4adaf761e349278e6fb73cd8c3c6ef9a97bb3208
Sharma Ji <Sharma Ji@DESKTOP-7NI4SEB.mshome.net>
2024-07-25 15:41:34 +0530

    modify

--- SampleGitHubProject/src/test/java/com/createorg/CreateOrgTest.java
+++ SampleGitHubProject/src/test/java/com/createorg/CreateOrgTest.java
@@ -11,7 +11,7 @@ public class CreateOrgTest {
 		System.out.println("<====Execute create organization test===>");
 		System.out.println("<====Execute create organization test===>");
 		System.out.println("<====Execute create organization test===>");
-		System.out.println("hi");
+
 	}
 
 }
