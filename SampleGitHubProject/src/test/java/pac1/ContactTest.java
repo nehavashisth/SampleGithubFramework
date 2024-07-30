eb8ca926309ab3e32da39c62961e836751b5218d
nehavashisth <nehavashisth59@gmail.com>
2024-07-30 14:37:51 +0530

    First Commit

---
+++ SampleGitHubProject/src/test/java/pac1/ContactTest.java
@@ -0,0 +1,26 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+
+	@Test
+	public void createcontacttest()
+	{
+		String url=System.getProperty("url");
+		String browser=System.getProperty("browser");
+		String username=System.getProperty("username");
+		String password=System.getProperty("password");
+		System.out.println(url);
+		System.out.println(browser);
+		System.out.println(username);
+		System.out.println(password);
+		System.out.println("execute createcontacttest");
+	}
+	
+	@Test
+	public void modifycontacttest()
+	{
+		System.out.println("execute modifycontacttest");
+	}
+}
---
+++ SampleGitHubProject/src/test/java/pac2/OrgTest.java
@@ -0,0 +1,18 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+
+	@Test
+	public void createOrgTest()
+	{
+		System.out.println("execute createOrgTest");
+	}
+	
+	@Test
+	public void modifyOrgttest()
+	{
+		System.out.println("execute modifyOrgttest");
+	}
+}
