243eec70ee6c326809995a9813f8dd358b0b73fb
nehavashisth <nehavashisth59@gmail.com>
2024-07-30 15:28:43 +0530

    added new test

--- SampleGitHubProject/src/test/java/pac1/ContactTest.java
+++ SampleGitHubProject/src/test/java/pac1/ContactTest.java
@@ -24,4 +24,10 @@ public class ContactTest {
 	{
 		System.out.println("execute modifycontacttest");
 	}
+	
+	@Test
+	public void deletecontacttest()
+	{
+		System.out.println("execute deletecontacttest");
+	}
 }
